48d1df544706e147d2722a76410b94ceed9d1884
Gabor Szarnyas <szarnyasg@gmail.com>
2020-06-12 15:08:39 +0200

    optimize imports

--- src/main/java/janusgraph/JanusGraphTest.java
+++ src/main/java/janusgraph/JanusGraphTest.java
@@ -1,13 +1,8 @@
 package janusgraph;
 
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.structure.Graph;
-import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class JanusGraphTest {
 
