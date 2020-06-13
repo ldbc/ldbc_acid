865d712793002fb55a149f0fbe913918255c6a1d
Gabor Szarnyas <szarnyasg@gmail.com>
2020-06-14 00:27:07 +0200

    PG close

--- src/test/java/test/PostgresTest.java
+++ src/test/java/test/PostgresTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import postgres.PostgresDriver;
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
@@ -10,15 +11,22 @@ import static ru.yandex.qatools.embed.postgresql.distribution.Version.Main.*;
 
 public class PostgresTest extends AcidTest<PostgresDriver> {
 
+    static EmbeddedPostgres postgres;
+
     public PostgresTest() {
         super(new PostgresDriver());
     }
 
     @BeforeClass
     public static void setUp() throws IOException {
-        final EmbeddedPostgres postgres = new EmbeddedPostgres(V9_6);
+        postgres = new EmbeddedPostgres(V9_6);
         final String url = postgres.start("localhost", 5432, "dbName", "userName", "password");
         // TODO
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        postgres.stop();
     }
 
 }
