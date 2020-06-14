346e784ab657a708147610c5396666d838c4e4ca
miratepuffin <piratemuffin@hotmail.co.uk>
2020-06-14 02:25:27 +0100

    Added JanusGraph g1c

--- src/main/java/janusgraph/JanusGraphDriver.java
+++ src/main/java/janusgraph/JanusGraphDriver.java
@@ -49,57 +49,15 @@ public class JanusGraphDriver extends TestDriver {
 
     }
 
-    //****** IMP BLOCK ******//
 
-    @Override
-    public void impInit() {
-        JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
-        Vertex v = gWrite.addV().next();
-        v.property("id",1L);
-        v.property("version",1L);
-        transaction.commit();
-    }
-
-    @Override
-    public Map<String, Object> imp1(Map parameters) {
-
-        JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
-        long personID = (long) parameters.get("personId");
-
-        if(gWrite.V().has("id",personID).hasNext()) {
-            Vertex currentVertex = gWrite.V().has("id", personID).next();
-            int currentVersion = currentVertex.value("version");
-            currentVertex.property("version", currentVersion + 1L);
-            commitTransaction(transaction);
-            return ImmutableMap.of();
-        }
-        else
-            throw new IllegalStateException("IMP1 Person Missing from Graph");
-
-    }
-
-    @Override
-    public Map<String, Object> imp2(Map parameters) {
-        JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
-        long personID  = (long) parameters.get("personId");
-        long sleepTime = (long) parameters.get("sleepTime");
-        int firstRead =  gWrite.V().has("id",personID).next().value("version");
-        sleep(sleepTime);
-        int secondRead = gWrite.V().has("id",personID).next().value("version");
-
-        return ImmutableMap.of("firstRead", (long) firstRead, "secondRead", (long) secondRead);
-    }
 
     //****** G1A BLOCK ******//
 
     @Override
     public void g1aInit() {
         JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
-        Vertex v = gWrite.addV().next();
+        GraphTraversalSource g = transaction.traversal();
+        Vertex v = g.addV().next();
         v.property("id",1L);
         v.property("version",1L);
         transaction.commit();
@@ -108,14 +66,14 @@ public class JanusGraphDriver extends TestDriver {
     @Override
     public Map<String, Object> g1a1(Map parameters) {
         JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
+        GraphTraversalSource g = transaction.traversal();
         long personID = (long) parameters.get("personId");
         long sleepTime = (long) parameters.get("sleepTime");
-        if(gWrite.V().has("id",personID).hasNext()) {
-            Vertex currentVertex = gWrite.V().has("id", personID).next();
-            int currentVersion = currentVertex.value("version");
+        if(g.V().has("id",personID).hasNext()) {
+            Vertex person = g.V().has("id", personID).next();
+            int currentVersion = person.value("version");
             sleep(sleepTime);
-            currentVertex.property("version", currentVersion + 1L);
+            person.property("version", currentVersion + 1L);
             sleep(sleepTime);
             abortTransaction(transaction);
             return ImmutableMap.of();
@@ -127,11 +85,11 @@ public class JanusGraphDriver extends TestDriver {
     @Override
     public Map<String, Object> g1a2(Map parameters) {
         JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
+        GraphTraversalSource g = transaction.traversal();
         long personID = (long) parameters.get("personId");
-        if(gWrite.V().has("id",personID).hasNext()) {
-            Vertex currentVertex = gWrite.V().has("id", personID).next();
-            int pVersion = currentVertex.value("version");
+        if(g.V().has("id",personID).hasNext()) {
+            Vertex person = g.V().has("id", personID).next();
+            int pVersion = person.value("version");
             return ImmutableMap.of("pVersion", (long) pVersion);
         }
         else
@@ -144,8 +102,8 @@ public class JanusGraphDriver extends TestDriver {
     @Override
     public void g1bInit() {
         JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
-        Vertex v = gWrite.addV().next();
+        GraphTraversalSource g = transaction.traversal();
+        Vertex v = g.addV().next();
         v.property("id",1L);
         v.property("version",99L);
         transaction.commit();
@@ -154,60 +112,124 @@ public class JanusGraphDriver extends TestDriver {
     @Override
     public Map<String, Object> g1b1(Map parameters) {
         JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
+        GraphTraversalSource g = transaction.traversal();
         long personID  = (long) parameters.get("personId");
         long sleepTime = (long) parameters.get("sleepTime");
         long even      = (long) parameters.get("even");
         long odd       = (long) parameters.get("odd");
 
-        if(gWrite.V().has("id",personID).hasNext()) {
-            Vertex currentVertex = gWrite.V().has("id", personID).next();
-            currentVertex.property("version", even);
+        if(g.V().has("id",personID).hasNext()) {
+            Vertex person = g.V().has("id", personID).next();
+            person.property("version", even);
             sleep(sleepTime);
-            currentVertex.property("version", odd);
+            person.property("version", odd);
             abortTransaction(transaction);
             return ImmutableMap.of();
         }
         else
-            throw new IllegalStateException("G1A1 Person Missing from Graph");
+            throw new IllegalStateException("G1B1 Person Missing from Graph");
     }
 
     @Override
     public Map<String, Object> g1b2(Map parameters) {
         JanusGraphTransaction transaction = startTransaction();
-        GraphTraversalSource gWrite = transaction.traversal();
+        GraphTraversalSource g = transaction.traversal();
         long personID = (long) parameters.get("personId");
-        if(gWrite.V().has("id",personID).hasNext()) {
-            Vertex currentVertex = gWrite.V().has("id", personID).next();
-            int pVersion = currentVertex.value("version");
+        if(g.V().has("id",personID).hasNext()) {
+            Vertex person = g.V().has("id", personID).next();
+            int pVersion = person.value("version");
             return ImmutableMap.of("pVersion", (long) pVersion);
         }
         else
-            throw new IllegalStateException("G2A2 Person Missing from Graph");
+            throw new IllegalStateException("G2B2 Person Missing from Graph");
+    }
+
+    //****** G1C BLOCK ******//
+    @Override
+    public void g1cInit() {
+        JanusGraphTransaction transaction = startTransaction();
+        GraphTraversalSource g = transaction.traversal();
+        Vertex v1 = g.addV().next();
+        v1.property("id",1L);
+        v1.property("version",0L);
+        Vertex v2 = g.addV().next();
+        v2.property("id",2L);
+        v2.property("version",0L);
+        transaction.commit();
     }
 
 
+    @Override
+    public Map<String, Object> g1c(Map parameters) {
+        JanusGraphTransaction transaction = startTransaction();
+        GraphTraversalSource g = transaction.traversal();
+        long person1ID      = (long) parameters.get("person1Id");
+        long person2ID      = (long) parameters.get("person2Id");
+        long transactionId  = (long) parameters.get("transactionId");
+
+        if(g.V().has("id",person1ID).hasNext() &&
+           g.V().has("id",person2ID).hasNext()) {
+            Vertex person1 = g.V().has("id", person1ID).next();
+            person1.property("version", transactionId);
+            Vertex person2 = g.V().has("id", person2ID).next();
+            int person2Version = person2.value("version");
+            commitTransaction(transaction);
+            return ImmutableMap.of("person2Version", person2Version);
+        }
+        else
+            throw new IllegalStateException("G1C Person Missing from Graph");
+    }
+
+
+    //****** IMP BLOCK ******//
+
+    @Override
+    public void impInit() {
+        JanusGraphTransaction transaction = startTransaction();
+        GraphTraversalSource g = transaction.traversal();
+        Vertex v = g.addV().next();
+        v.property("id",1L);
+        v.property("version",1L);
+        transaction.commit();
+    }
+
+    @Override
+    public Map<String, Object> imp1(Map parameters) {
+
+        JanusGraphTransaction transaction = startTransaction();
+        GraphTraversalSource g = transaction.traversal();
+        long personID = (long) parameters.get("personId");
+
+        if(g.V().has("id",personID).hasNext()) {
+            Vertex person = g.V().has("id", personID).next();
+            int currentVersion = person.value("version");
+            person.property("version", currentVersion + 1L);
+            commitTransaction(transaction);
+            return ImmutableMap.of();
+        }
+        else
+            throw new IllegalStateException("IMP1 Person Missing from Graph");
+
+    }
+
+    @Override
+    public Map<String, Object> imp2(Map parameters) {
+        JanusGraphTransaction transaction = startTransaction();
+        GraphTraversalSource g = transaction.traversal();
+        long personID  = (long) parameters.get("personId");
+        long sleepTime = (long) parameters.get("sleepTime");
+        int firstRead =  g.V().has("id",personID).next().value("version");
+        sleep(sleepTime);
+        int secondRead = g.V().has("id",personID).next().value("version");
+
+        return ImmutableMap.of("firstRead", (long) firstRead, "secondRead", (long) secondRead);
+    }
 
 
     @Override
     public void g0Init() {
 
     }
-
-
-
-
-
-    @Override
-    public void g1cInit() {
-
-    }
-
-    @Override
-    public Map<String, Object> g1c(Map parameters) {
-        return null;
-    }
-
 
     @Override
     public void pmpInit() {
