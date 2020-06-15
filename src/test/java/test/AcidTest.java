9e2ee344a59263f4a24a6c1963706e22c3c7cdf6
Gabor Szarnyas <szarnyasg@gmail.com>
2020-06-15 23:16:28 +0200

    fix G0, add assertions

--- src/test/java/test/AcidTest.java
+++ src/test/java/test/AcidTest.java
@@ -2,17 +2,18 @@ package test;
 
 import com.google.common.collect.ImmutableMap;
 import driver.TestDriver;
-import org.junit.Assert;
-import transactions.TransactionThread;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import transactions.TransactionThread;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -72,6 +73,9 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
         final long numKnowsEdges = (long) results.get("numKnowsEdges");
         final boolean pass = ((nTransactions == numFriendsProp)&&(nTransactions == numKnowsEdges));
         System.out.printf("LU:    %4d %4d %4d %5b\n", nTransactions, numFriendsProp, numKnowsEdges, pass);
+
+        Assert.assertEquals(nTransactions, numFriendsProp);
+        Assert.assertEquals(nTransactions, numKnowsEdges);
     }
 
     @Test
@@ -91,15 +95,27 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
 
         Map<String, Object> results = testDriver.g0check(ImmutableMap.of("person1Id", 1L, "person2Id", 2L));
         if (results.containsKey("p1VersionHistory")) {
-            final List<Long> p1VersionHistory = (List<Long>) results.get("p1VersionHistory");
-            final List<Long> kVersionHistory  = (List<Long>) results.get("kVersionHistory" );
-            final List<Long> p2VersionHistory = (List<Long>) results.get("p2VersionHistory");
+            final List<Long> p1VersionHistory = new ArrayList<>((List<Long>) results.get("p1VersionHistory"));
+            final List<Long> kVersionHistory  = new ArrayList<>((List<Long>) results.get("kVersionHistory" ));
+            final List<Long> p2VersionHistory = new ArrayList<>((List<Long>) results.get("p2VersionHistory"));
+
+            p1VersionHistory.retainAll(kVersionHistory);
+            p1VersionHistory.retainAll(p2VersionHistory);
+
+            kVersionHistory.retainAll(p1VersionHistory);
+            kVersionHistory.retainAll(p2VersionHistory);
+
+            p2VersionHistory.retainAll(p1VersionHistory);
+            p2VersionHistory.retainAll(kVersionHistory);
 
             System.out.printf("G0:    %s %s %s %5b %5b %5b\n",
                     p1VersionHistory, kVersionHistory, p2VersionHistory,
                     p1VersionHistory.equals(kVersionHistory),
                     p1VersionHistory.equals(p2VersionHistory),
                     kVersionHistory.equals(p1VersionHistory));
+            Assert.assertEquals(p1VersionHistory, kVersionHistory);
+            Assert.assertEquals(p1VersionHistory, p2VersionHistory);
+            Assert.assertEquals(kVersionHistory, p1VersionHistory);
         }
     }
 
@@ -124,6 +140,7 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
                 final long pVersion = (long) results.get("pVersion");
 
                 System.out.printf("G1a:   %4d %4d %5b\n", 1L, pVersion, 1L == pVersion);
+                Assert.assertEquals(1L, pVersion);
             }
         }
     }
@@ -148,7 +165,8 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
             if (results.containsKey("pVersion")) {
                 final long pVersion = (long) results.get("pVersion");
 
-                System.out.printf("G1b:   %4d %4d %5b\n", 1L, pVersion, 1L == pVersion);
+                System.out.printf("G1b:   %4d %4d %5b\n", 1L, pVersion, pVersion % 2 == 1);
+                Assert.assertTrue(pVersion % 2 == 1);
             }
         }
     }
@@ -156,33 +174,34 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
     @Test
     public void g1cTest() throws Exception {
         testDriver.g1cInit();
-        final int c = 10;
+        final int c = 100;
 
         List<TransactionThread<Map<String, Object>, Map<String, Object>>> clients = new ArrayList<>();
-        for (long i = 0; i < c; i++) {
-            clients.add(new TransactionThread<>(i, testDriver::g1c, ImmutableMap.of("person1Id", 1L, "person2Id", 2L, "transactionId", i)));
+        final Random random = new Random();
+        for (long i = 1; i <= c; i++) {
+            final boolean order = random.nextBoolean();
+            long person1Id = order ? 1L : 2L;
+            long person2Id = order ? 2L : 1L;
+            clients.add(new TransactionThread<>(i, testDriver::g1c, ImmutableMap.of("person1Id", person1Id, "person2Id", person2Id, "transactionId", i)));
         }
 
         final List<Future<Map<String, Object>>> futures = executorService.invokeAll(clients);
-
         List<Map<String, Object>> resultss = new ArrayList<>();
         for (Future<Map<String, Object>> future : futures) {
             resultss.add(future.get());
         }
-//        for (int i = 0; i < c; i++) {
-//            resultss.add(futures.get(i).get());
-//        }
 
-        for (int i = 0; i < c; i++) {
-            long transactionId = clients.get(i).getTransactionId();
-            Map<String, Object> results = resultss.get(i);
+        for (int i = 1; i <= c; i++) {
+            Map<String, Object> results1 = resultss.get(i-1);
+            final int person2Version1 = ((Long) results1.get("person2Version")).intValue();
 
-            final long person2Version = (long) results.get("person2Version");
-            Map<String, Object> results2 = resultss.get((int) person2Version);
+            if (person2Version1 == 0L) continue;
 
-            //long person2Version2 = (long) results2.get("person2Version");
+            final Map<String, Object> results2 = resultss.get(person2Version1-1);
+            final int person2Version2 = ((Long) results2.get("person2Version")).intValue();
 
-            System.out.printf("G1c:   %4d %4d %5b\n", person2Version, 1, person2Version != 1);
+            System.out.printf("G1c:   %4d %4d %4d %5b\n", i, person2Version1, person2Version2, i != person2Version2);
+            Assert.assertNotEquals(i, person2Version2);
         }
     }
 
@@ -207,6 +226,7 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
                 final long firstRead = (long) results.get("firstRead");
                 final long secondRead = (long) results.get("secondRead");
                 System.out.printf("IMP:   %4d %4d %5b\n", firstRead, secondRead, firstRead == secondRead);
+                Assert.assertEquals(firstRead, secondRead);
             }
         }
     }
@@ -232,6 +252,7 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
                 final long firstRead = (long) results.get("firstRead");
                 final long secondRead = (long) results.get("secondRead");
                 System.out.printf("PMP:   %4d %4d %5b\n", firstRead, secondRead, firstRead == secondRead);
+                Assert.assertEquals(firstRead, secondRead);
             }
         }
     }
@@ -257,6 +278,7 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
                 final Set<Long> firstRead  = new HashSet<>((List<Long>) results.get("firstRead"));
                 final Set<Long> secondRead = new HashSet<>((List<Long>) results.get("secondRead"));
                 System.out.printf("FR:   %4s %4s %5b\n", firstRead, secondRead, !firstRead.equals(secondRead));
+                Assert.assertNotEquals(firstRead, secondRead);
             }
         }
     }
@@ -279,6 +301,8 @@ public abstract class AcidTest<TTestDriver extends TestDriver> {
                 final List<Long> firstRead  = ((List<Long>) results.get("firstRead" ));
                 final List<Long> secondRead = ((List<Long>) results.get("secondRead"));
                 System.out.printf("OTV:   %4s %4s %5b\n", firstRead, secondRead, Collections.max(firstRead) <= Collections.min(secondRead));
+
+                Assert.assertTrue(Collections.max(firstRead) <= Collections.min(secondRead));
             }
         }
     }
