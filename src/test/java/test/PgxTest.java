374f547f48d6923cf804df8e89296523f7651cc4
Gabor Szarnyas <szarnyasg@gmail.com>
2020-06-17 02:08:35 +0200

    Add initial pgx code

---
+++ src/test/java/test/PgxTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import oracle.pgx.api.PgxGraph;
+import org.junit.Test;
+
+public class PgxTest {
+
+    @Test
+    public void test() {
+        System.out.println("PGX");
+        PgxGraph graph = null;
+    }
+
+}
