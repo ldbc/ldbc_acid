b353945476f0c2adf76786ad169089643647b56f
denitsa <denitsa.stoyanova@ontotext.com>
2022-10-07 11:08:23 +0300

    GDB-7368: Introduce repository creation using RemoteRepositoryManager

--- src/test/java/test/GraphDBAcidTest.java
+++ src/test/java/test/GraphDBAcidTest.java
@@ -4,6 +4,6 @@ import graphdb.GraphDBDriver;
 
 public class GraphDBAcidTest extends AcidTest<GraphDBDriver> {
 	public GraphDBAcidTest() {
-		super(new GraphDBDriver("http://localhost:7201/repositories/ldbc-snb-acid-test"));
+		super(new GraphDBDriver("http://localhost:7200"));
 	}
 }
